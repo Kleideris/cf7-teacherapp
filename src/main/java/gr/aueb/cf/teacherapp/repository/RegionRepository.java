8cf7aa3c69d54101fb2a881c63b9f7dc3b58ee97
Ch. Kleideris <kleiderisch@gmail.com>
2025-05-29 18:42:52 +0300

    chore: clear

--- src/main/java/gr/aueb/cf/teacherapp/repository/RegionRepository.java
+++ src/main/java/gr/aueb/cf/teacherapp/repository/RegionRepository.java
@@ -3,8 +3,8 @@ package gr.aueb.cf.teacherapp.repository;
 import gr.aueb.cf.teacherapp.model.static_data.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
+//import org.springframework.stereotype.Repository;
 
-@Repository
+//@Repository
 public interface RegionRepository extends JpaRepository<Region, Long>, JpaSpecificationExecutor<Region> {
 }
